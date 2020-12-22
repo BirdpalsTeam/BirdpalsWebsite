ac49589bc9b905d9b133bfe9af8ee8f96b1904c6
FarawayDrip30 <66689307+FarawayDrip30@users.noreply.github.com>
2020-12-22 17:59:57 +0000

    Add files via upload

---
+++ GameData/Scripts/Client.js
@@ -0,0 +1,100 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+
+class Room
+{
+
+	constructor(img, x, y, width, height)
+	{
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	draw(){
+		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+	}
+
+}
+
+class Character
+{
+	
+	constructor(img, x, y, width, height)
+	{
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		this.velX = 0;
+		this.velY = 0;
+
+		this.destX = x;
+		this.destY = y;
+	}
+
+	draw()
+	{
+		
+		ctx.drawImage(this.img, this.x - this.width/2, this.y - this.height + 10, this.width, this.height);
+
+		if((this.x >= this.destX-1 && this.x <= this.destX+1 && this.y >= this.destY-1 && this.y <= this.destY+1) == false){
+			this.x += this.velX;
+			this.y += this.velY;
+		}
+	}
+
+	move(destX, destY){
+		this.destX = destX;
+		this.destY = destY;
+		
+		let dx = destX - this.x;
+		let dy = destY - this.y;
+
+		let angle = Math.atan2(dy, dx);
+
+		let speed = 1;
+		this.velX = Math.cos(angle) * speed;
+		this.velY = Math.sin(angle) * speed;
+	}
+
+}
+
+var charImg = new Image();
+charImg.src = "GameData/Sprites/bluebird.png";
+
+var roomImg = new Image();
+roomImg.src = "GameData/Sprites/room1.png"
+
+var room = new Room(roomImg, 0, 0, canvas.width, canvas.height);
+var char = new Character(charImg, 40, 40, 62, 72);
+
+function main(){
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	room.draw();
+	char.draw();
+
+}
+
+function getMousePos(canvas, e) {
+    let rect = canvas.getBoundingClientRect();
+
+	char.move(e.clientX - rect.left, e.clientY - rect.top);
+
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
+  }
+  canvas.addEventListener('click', function(e) {
+    let mousePos = getMousePos(canvas, e);
+    console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+  }, false);
+
+setInterval(main, 10)
