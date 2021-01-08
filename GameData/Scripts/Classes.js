0fc1dc4f31e92b205a0e408c42a857f88f3c261c
flines <37344174+Fl1nns@users.noreply.github.com>
2021-01-07 21:16:27 -0300

    message go bye bi

--- GameData/Scripts/Classes.js
+++ GameData/Scripts/Classes.js
@@ -272,7 +272,7 @@ class Character extends GameObject {
      this.message = msg;
    }
   
-  setInterval(chatMessage(" "), 5000);
+  setInterval(if(this.message.length > 0) { this.message = " "; }, 5000);
 }
 
 class NPC extends GameObject {
