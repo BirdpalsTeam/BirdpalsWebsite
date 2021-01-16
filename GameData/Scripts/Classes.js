d9d3a1f674ff9e8d2b1a12875114a360a059bc70
flines <37344174+Fl1nns@users.noreply.github.com>
2021-01-16 17:27:44 -0300

    b final

--- GameData/Scripts/Classes.js
+++ GameData/Scripts/Classes.js
@@ -101,7 +101,7 @@ class GameObject {
         ctx.fillText(
           this.message,
           this.x - ctx.measureText(this.message).width / 2,
-          this.y - this.height - 16,
+          this.y - this.height - 13,
         );
       }
     }
