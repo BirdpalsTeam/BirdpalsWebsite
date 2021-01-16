e86ef2d5a322bab6b8f03d971f7f6d7c4c76a2af
flines <37344174+Fl1nns@users.noreply.github.com>
2021-01-16 17:25:49 -0300

    Update Classes.js

--- GameData/Scripts/Classes.js
+++ GameData/Scripts/Classes.js
@@ -83,7 +83,7 @@ class GameObject {
       if (this.message != " " && this.message.length > 0 || this.message != "" && this.message.length > 0) {
         var bubble_image = new HUD(
            bubble,
-           this.x - 65,
+           this.x - 70,
            this.y - this.height - 35,
            131,
            47,
@@ -101,7 +101,7 @@ class GameObject {
         ctx.fillText(
           this.message,
           this.x - ctx.measureText(this.message).width / 2,
-          this.y - this.height - 10,
+          this.y - this.height - 25,
         );
       }
     }
